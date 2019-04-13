677914a64d36ae25b4de81e804c3addf5d2d480e
Chagil Guiab <chagilguiab@gmail.com>
2019-04-13 15:21:01 -0700

    renames targer div in Druth-service

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,4 +3,4 @@ import { render } from 'react-dom';
 import App from './components/App.jsx';
 
 window.ImageCarousel = App;
-render(<App />, document.getElementById('app') || document.createElement('div'));
+render(<App />, document.getElementById('Images') || document.createElement('div'));
